f3d65448247baab8ff98fef4f3ae3831068609c3
Mikkel RINGAUD <mikkel@milescode.dev>
2024-03-05 19:18:01 +0100

    feat(TP3): add ex6

--- TP3/ex6.c
+++ TP3/ex6.c
@@ -1,115 +1,108 @@
-#include<stdio.h>
+#include <stdio.h>
 
-/**
- * Écrire un utilitaire affiche affichant sur stdout les arguments de la ligne de commande.
- * Chaque argument sera affiché sur une ligne séparé.
- * 
- * SYNTAXE: affiche [ -r | -M | -m ] arg1 [arg...]
- * 
- * Les crochets signifient "élément optionnel"
- * La barre verticale signifie "ou bien"
- * 
- * -r : Affiche les arguments dans l'ordre inverse
- * -M : Affiche les arguments en majuscule
- * -n : Affiche les arguments en mettant en majuscule uniquement le premier caractère
- */
-void main(int argc, char *argv[]) {
-  if (argc < 2) {
-    printf("Erreur: ce programme attend au moins 1 argument.\n");
-    return;
+char toUpper (char chr) {
+  if (chr >= 97 && chr <= 122) {
+    return chr - 32;
   }
 
-  if (argc == 2) {
-    printf("%s\n", argv[1]);
-    return;
-  }
+  return chr;
+}
 
-  int skip_index_args = 0;
-  int reverse = 0;
-  int upper = 0;
-  int upperFirstChar = 0;
-  if (argc >= 3) {
+int main (int argc, char *argv[]) {
+  char **ptr;
+  char *chr;
 
-    // shouldnt accept options in the args
-    for (int i = 1; i < argc; i++) {
-      if (argv[i][0] == '-') {
-        if (argv[i][1] == 'r') {
-          reverse = 1;
-        } else if (argv[i][1] == 'M') {
-          upper = 1;
-        } else if (argv[i][1] == 'm') {
-          upperFirstChar = 1;
-        } else {
-          printf("Erreur: option inconnue.\n");
-          return;
-        }
-        skip_index_args++;
-      }
+  /**
+   * 0 = output as is
+   * 1 = inverted (-r)
+   * 2 = uppercase (-M)
+   * 3 = uppercase only first letter (-m)
+   * 
+   * -1 = invalid
+   */
+  int mode = 0;
+
+  // set ourselves on the first argument
+  ptr = argv + 1;
+  if (ptr == NULL) return 0; // empty
+
+  // set ourselves on the first letter of the first argument
+  chr = *ptr;
+  if (*chr == '\0') return 0; // empty
+
+  // parameter has been set, let's check what it is
+  if (*chr == '-') {
+    switch (*(chr + 1)) {
+      case 'r':
+        mode = 1;
+        break;
+      case 'M':
+        mode = 2;
+        break;
+      case 'm':
+        mode = 3;
+        break;
+      default:
+        mode = -1;
     }
+
+    if (mode == -1) {
+      printf("error: unknown parameter has been passed");
+      return 1;
+    }
+
+    // parameter only have one character, so fail if more are given.
+    if (*(chr + 2) != '\0') {
+      printf("error: parameter should only have one character.");
+      return 2;
+    }
+
+    // set ptr to next argument
+    ptr++;
   }
 
-  if (argc - skip_index_args == 2) {
-    printf("%s\n", argv[1 + skip_index_args]);
-    return;
-  }
+  if (mode == 1) {
+    char **start_ptr;
 
-  if (reverse == 0) {
-    for (int i = 1 + skip_index_args; i < argc; i++) {
-      if (upper) {
-        for (int j = 0; argv[i][j] != '\0'; j++) {
-          if (argv[i][j] >= 'a' && argv[i][j] <= 'z') {
-            printf("%c", argv[i][j] - 32);
-          } else {
-            printf("%c", argv[i][j]);
-          }
-        }
-        printf("\n");
-      }
-      else if (upperFirstChar) {
-        for (int j = 0; argv[i][j] != '\0'; j++) {
-          if (j == 0) {
-            if (argv[i][j] >= 'A' && argv[i][j] <= 'Z') {
-              printf("%c", argv[i][j] + 32);
-            } else {
-              printf("%c", argv[i][j]);
-            }
-          } else {
-            printf("%c", argv[i][j]);
-          }
-        }
+    // set ptr at the end of the arguments list
+    // keep start_ptr at the current position
+    for (start_ptr = ptr; *ptr != NULL; ptr++);
 
-        printf("\n");
-      }
+    // loop backwards
+    for (ptr--; ptr != start_ptr - 1; ptr--) {
+      printf("%s\n", *ptr);
     }
   }
   else {
-    for (int i = argc - 1; i > 0; i--) {
-      if (upper) {
-        for (int j = 0; argv[i][j] != '\0'; j++) {
-          if (argv[i][j] >= 'a' && argv[i][j] <= 'z') {
-            printf("%c", argv[i][j] - 32);
-          } else {
-            printf("%c", argv[i][j]);
-          }
-        }
-        printf("\n");
-      }
-      else if (upperFirstChar) {
-        for (int j = 0; argv[i][j] != '\0'; j++) {
-          if (j == 0) {
-            if (argv[i][j] >= 'A' && argv[i][j] <= 'Z') {
-              printf("%c", argv[i][j] + 32);
-            } else {
-              printf("%c", argv[i][j]);
-            }
-          } else {
-            printf("%c", argv[i][j]);
-          }
+    for (; *ptr != NULL; ptr++) {
+      // when we output as is, just use %s
+      if (mode == 0) printf("%s\n", *ptr);
+      else {
+        // set the current character ptr to first character
+        chr = *ptr;
+
+        if (mode == 3) {
+          // uppercase the first character
+          *chr = toUpper(*chr);
+          printf("%c", *chr);
+
+          // go to next character (for the loop)
+          chr++;
         }
 
+        for (; *chr != '\0'; chr++) {
+          // uppercase every letter
+          if (mode == 2) printf("%c", toUpper(*chr));
+          
+          // print the character as is
+          else if (mode == 3) printf("%c", *chr);
+        }
+
+        // break line
         printf("\n");
       }
     }
   }
 
+  return 0;
 }
